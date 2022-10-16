a9a42ed1a96c92740d738afc5641deedcd0aac76
gungfu2020 <107550604+gungfu2020@users.noreply.github.com>
2022-10-16 18:24:08 +0800

    Update conf.sh

--- bin/conf.sh
+++ bin/conf.sh
@@ -4,6 +4,8 @@ cat << EOF > /app/Caddyfile
 :$PORT
 reverse_proxy /ray 127.0.0.1:8089
 EOF
+chmod +x /app/caddy
+chmod +x /app/helloworld
 echo "Starting caddy..."
 /app/caddy start -config /app/Caddyfile
 echo "Starting helloworld..."
