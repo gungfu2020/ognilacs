5c8319231adcdc07600dead855b2b9dc24fb119a
gungfu2020 <107550604+gungfu2020@users.noreply.github.com>
2022-10-16 11:24:23 +0800

    Add files via upload

---
+++ main.go
@@ -0,0 +1,105 @@
+package main
+
+import (
+	"flag"
+	"os"
+	"net/http"
+	"io"
+	"log"
+	"fmt"
+	"time"
+	"encoding/json"
+	"pikpak-upload-server/model"
+	"github.com/sirupsen/logrus"
+)
+
+var PATH = "/tmp/"
+
+func main() {
+	h1 := func(w http.ResponseWriter, _ *http.Request) {
+		io.WriteString(w, "Hello from a HandleFunc #1!\n")
+	}
+	http.HandleFunc("/", h1)
+	http.HandleFunc("/api", h2)
+	PORT := ":" + os.Getenv("PORT")
+	log.Println("listen port: ", PORT)
+	log.Fatal(http.ListenAndServe(PORT, nil))
+}
+
+func h2(w http.ResponseWriter, r *http.Request) {
+	type Args struct {
+		Fn   string `json:"fn"`
+		Link string `json:"link"`
+	}
+	var args Args
+	rbody, _ := io.ReadAll(r.Body)
+	defer r.Body.Close()
+	json.Unmarshal(rbody[0:], &args)
+	go job(args.Fn, args.Link)
+	io.WriteString(w, "good lucky\n")
+}
+
+func job(fn, link string) {
+	//download file
+	defer os.Remove(PATH+fn)
+	f, err := os.OpenFile(PATH+fn, os.O_RDWR|os.O_CREATE, 0755)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	var buf [32 * 1024 * 1024]byte
+	log.Println("download start ...")
+	res, err := http.Get(link)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	for {
+		n, err := res.Body.Read(buf[0:])
+		if err != nil && err != io.EOF {
+			log.Println(err)
+			return
+		}
+		//write to file
+		f.Write(buf[0:n])
+		if err == io.EOF {
+			break
+		}
+	}
+	t := fmt.Sprintf("%d",time.Now().Unix())
+	f.Write([]byte(t))
+	f.Close()
+	log.Println("download completed!")
+	//upload file
+	p := model.NewPikPak("gungfu2022@hotmail.com", "888888abc")
+	err = p.Login()
+	if err != nil {
+		logrus.Error(err)
+	}
+	err = p.AuthCaptchaToken("POST:/drive/v1/files")
+	if err != nil {
+		logrus.Error(err)
+	}
+	logrus.Infoln("upload start...")
+	err = p.UploadFile(parentId, PATH+fn)
+	if err != nil {
+		logrus.Error(err)
+	} else {
+		logrus.Infoln("upload completed!")
+	}
+}
+
+var parentId = ""
+
+func init() {
+	/*err := conf.InitConfig()
+	if err != nil {
+		logrus.Error(err)
+	}*/
+	parentid := flag.String("p", "", "ParentId")
+	concurrent := flag.Int("c", 4, "Concurrent")
+	flag.Parse()
+	parentId = *parentid
+	model.Concurrent = int64(*concurrent)
+}
+
