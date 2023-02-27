eb1f6cf005f1039b54459920cd4d58acbd9253dc
gungfu2012 <gungfu2012@hotmail.com>
2023-02-27 21:18:38 +0800

    update

--- main.go
+++ main.go
@@ -16,11 +16,15 @@ import (
 var PATH = "/tmp/"
 
 func main() {
+	log_file, _ := os.OpenFile("/tmp/pikpak_log", os.O_WRONLY|os.O_CREATE|os.O_APPEND|os.O_SYNC, 0644)
+	logrus.SetOutput(log_file)
+	log.SetOutput(log_file)
 	h1 := func(w http.ResponseWriter, _ *http.Request) {
 		io.WriteString(w, "Hello from a HandleFunc #1!\n")
 	}
 	http.HandleFunc("/", h1)
 	http.HandleFunc("/api", h2)
+	http.HandleFunc("/log", h3)
 	PORT := ":" + os.Getenv("PORT")
 	log.Println("listen port: ", PORT)
 	log.Fatal(http.ListenAndServe(PORT, nil))
@@ -43,6 +47,7 @@ func job(fn, link string) {
 	//download file
 	defer os.Remove(PATH+fn)
 	f, err := os.OpenFile(PATH+fn, os.O_RDWR|os.O_CREATE, 0755)
+	defer f.Close()
 	if err != nil {
 		log.Println(err)
 		return
@@ -61,7 +66,11 @@ func job(fn, link string) {
 			return
 		}
 		//write to file
-		f.Write(buf[0:n])
+		_, f_err := f.Write(buf[0:n])
+		if f_err != nil {
+			log.Println(f_err)
+			return
+		}
 		if err == io.EOF {
 			break
 		}
@@ -103,3 +112,11 @@ func init() {
 	model.Concurrent = int64(*concurrent)
 }
 
+func h3(w http.ResponseWriter, r *http.Request) {
+	data, err := os.ReadFile("/tmp/pikpak_log")
+	if err != nil {
+		fmt.Println(err)
+		io.WriteString(w, err.Error())
+	}
+	w.Write(data)
+}
